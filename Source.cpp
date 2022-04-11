1283fe2a16c21011b7ff5f9487d74908cc62d218
0xF5T9 <truyenhaunhan@gmail.com>
2022-04-11 14:04:06 +0700

    update comment

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,6 @@
 ﻿#include <iostream>		//	Input Outut Stream - C++ Console library
 #include <Windows.h>	// Windows - Windows library
-#include <string>		// String - C++ basic library test
+#include <string>		// String - C++ basic library
 
 using namespace std;
 
